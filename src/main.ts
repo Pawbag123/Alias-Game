20c902a88d5e399ee144c64ebc7a7c83c3f75102
JuanMPaola <juanmpaola25@gmail.com>
2024-09-27 10:58:22 -0300

    Dependencies problems solvd and front start + database merged

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-<<<<<<< HEAD
+
 import { ValidationPipe } from '@nestjs/common';
-=======
+
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
->>>>>>> a7928665cfc8d0d161a001d00616620c9eb02e38
+
 
 dotenv.config();
 
 async function bootstrap() {
-<<<<<<< HEAD
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-=======
+  
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
   //Setting handlebars view engine
   app.setViewEngine('hbs');
   app.setBaseViewsDir(join(__dirname, '..', 'src', 'views'));
   app.useStaticAssets(join(__dirname, '..', 'src', 'public'));
 
->>>>>>> a7928665cfc8d0d161a001d00616620c9eb02e38
   await app.listen(3000);
 }
 bootstrap();
